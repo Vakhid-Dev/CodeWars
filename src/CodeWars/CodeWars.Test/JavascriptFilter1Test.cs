908822908629d127d837aadd66087717cf8d2ef1
v.guliyev <v.guliyev@VGULIYEV>
2023-05-29 20:27:26 +0300

    ADDED JavascriptFilter1_7

---
+++ src/CodeWars/CodeWars.Test/JavascriptFilter1Test.cs
@@ -0,0 +1,31 @@
+﻿using CodeWars.Lib;
+
+namespace CodeWars.Test;
+
+public class JavascriptFilter1Test
+{
+   
+    [Test]
+    public void Test1()
+    {
+        string[][] a = { new[] { "foo", "foo@foo.com" }, new[] { "bar_", "bar@bar.com" } };
+        string[][] b = { new[] { "bar_", "bar@bar.com" } };
+        Assert.That(b,Is.EquivalentTo(JavascriptFilter1.search_names(a)));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        string[][] a = { new[] { "foo_", "foo@foo.com" }, new[] { "bar_", "bar@bar.com" } };
+        string[][] b = { new[] { "foo_", "foo@foo.com" }, new[] { "bar_", "bar@bar.com" } };
+        Assert.That(b, Is.EquivalentTo( JavascriptFilter1.search_names(a)));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        string[][] a = { new[] { "foo", "foo@foo.com" }, new[] { "bar", "bar@bar.com" } };
+        string[] b = new string[0];
+        Assert.That(b, Is.EquivalentTo( JavascriptFilter1.search_names(a)));
+    }
+}
