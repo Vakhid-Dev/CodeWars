0a46d8baadd91a34beabc14e29e51e7a9881b20a
v.guliyev <v.guliyev@VGULIYEV>
2023-05-30 22:45:12 +0300

    ADDED  CompleteThePattern__6_Odd_Ladder_7

---
+++ src/CodeWars/CodeWars.Test/CompleteThePattern__6_Odd_LadderTest.cs
@@ -0,0 +1,12 @@
+﻿using CodeWars.Lib;
+
+namespace CodeWars.Test;
+
+public class CompleteThePattern__6_Odd_LadderTest
+{
+    [Test]
+    public void Test_3()
+    {
+        Assert.AreEqual("1\n333", CompleteThePattern__6_Odd_Ladder.OddLadder(1), "Nope!");
+    }
+}
