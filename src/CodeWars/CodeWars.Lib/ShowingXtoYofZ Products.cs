bd395bd8bb769d666929888e9220f671abd95d49
v.guliyev <v.guliyev@VGULIYEV>
2023-05-30 19:11:15 +0300

    ADDED ShowingXtoYofZ_Products_7

---
+++ src/CodeWars/CodeWars.Lib/ShowingXtoYofZ Products.cs
@@ -0,0 +1,39 @@
+﻿namespace CodeWars.Lib;
+
+public class ShowingXtoYofZ_Products
+{
+    /// <summary>
+    /// https://www.codewars.com/kata/545cff101288c1d2da0006fb/train/csharp
+    /// </summary>
+    /// <param name="pageNumber"></param>
+    /// <param name="pageSize"></param>
+    /// <param name="totalProducts"></param>
+    /// <returns></returns>
+    public static string PaginationText(int pageNumber, int pageSize, int totalProducts)
+    {
+        var number = totalProducts;
+        var a = 1;
+        var b = 0;
+       
+        for (int i = 0; i < pageNumber; i++)
+        {
+            number -= pageSize;
+            
+            switch (number)
+            {
+                case <= 0 when i==0:
+                    return $"Showing {1} to {totalProducts} of {totalProducts} Products.";
+                case <= 0:
+                    a = b+1;
+                    b = a + (pageSize+number)-1;
+                    return $"Showing {a} to {b} of {totalProducts} Products.";
+                default:
+                    a = b+1;
+                    b += pageSize;
+                    break;
+            }
+        }
+        return $"Showing {a} to {b} of {totalProducts} Products.";
+    }
+    
+}
---
+++ src/CodeWars/CodeWars.Test/ShowingXtoYofZ ProductsTest.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars.Lib;
+
+namespace CodeWars.Test;
+
+public class ShowingXtoYofZ_ProductsTest
+{
+    [Test]
+    [TestCase(1,10,30,ExpectedResult="Showing 1 to 10 of 30 Products.")]
+    [TestCase(3,10,26,ExpectedResult="Showing 21 to 26 of 26 Products.")]
+    [TestCase(1,10,8,ExpectedResult="Showing 1 to 8 of 8 Products.")]
+    public static string FixedTest(int pageNumber, int pageSize, int totalProducts)
+    {
+        return ShowingXtoYofZ_Products.PaginationText(pageNumber, pageSize, totalProducts);
+    }
+}
