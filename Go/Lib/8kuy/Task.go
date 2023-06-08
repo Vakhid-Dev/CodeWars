b59485f6b1f3c463e1efdba72fac67a5990f66dc
v.guliyev <v.guliyev@VGULIYEV>
2023-06-08 17:45:39 +0300

    Added files

--- Go/Lib/8kuy/Task.go
+++ Go/Lib/8kuy/Task.go
@@ -144,3 +144,29 @@ func Invert(arr []int) []int {
 	}
 	return res
 }
+
+func MergeArrays(arr1, arr2 []int) []int {
+	var res []int
+	for i := 0; i < len(arr1); i++ {
+		if !contains(res, arr1[i]) {
+			res = append(res, arr1[i])
+		}
+	}
+	//for arr2
+	for i := 0; i < len(arr2); i++ {
+		if !contains(res, arr2[i]) {
+			res = append(res, arr2[i])
+		}
+	}
+	sort.Ints(res)
+	return res
+}
+
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
