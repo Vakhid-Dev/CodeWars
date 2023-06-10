8a0781b723a598fe7b9a401b9072965b764bf6d7
v.guliyev <v.guliyev@VGULIYEV>
2023-06-10 13:41:37 +0300

    6kuy_SortArray

---
+++ src/CodeWars/C_#/CodeWars.Test/SorttheoddTest.cs
@@ -0,0 +1,14 @@
+﻿using CodeWars.Lib;
+
+namespace CodeWars.Test;
+
+public class SorttheoddTest
+{
+    [Test]
+    public void BasicTests()
+    {
+        Assert.AreEqual(new int[] { 1, 3, 2, 8, 5, 4 }, Sorttheodd.SortArray(new int[] { 5, 3, 2, 8, 1, 4 }));
+        Assert.AreEqual(new int[] { 1, 3, 5, 8, 0 }, Sorttheodd.SortArray(new int[] { 5, 3, 1, 8, 0 }));
+        Assert.AreEqual(new int[] { }, Sorttheodd.SortArray(new int[] { }));
+    }
+}
