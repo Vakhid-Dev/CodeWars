0b589d8c4eb838878b668fa026c5e2bb5c8f0421
Vakhid-Dev <01.unbroken@gmail.com>
2023-07-28 17:00:06 +0300

    ArrayLowerCase

---
+++ src/CodeWars/C_#/CodeWars.Lib/Lowercasestringsinarray.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeWars.Lib;
+
+public class Lowercasestringsinarray
+{
+    public static object[] ArrayLowerCase(object[] arr)
+    {
+        return arr.Select(x => x is string ? x.ToString().ToLower() : x ).ToArray();;
+    }
+    /// <summary>
+    /// Good solution
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public static object[] ArrayLowerCase2(object[] arr)
+    {
+        return arr.Select(o => (o as string)?.ToLower() ?? o).ToArray();
+    }
+}
---
+++ src/CodeWars/C_#/CodeWars.Test/LowercasestringsinarrayTest.cs
@@ -0,0 +1,12 @@
+﻿using CodeWars.Lib;
+
+namespace CodeWars.Test;
+
+public class LowercasestringsinarrayTest
+{
+    [Test]
+    public void BasicTest()
+    {    
+        Assert.AreEqual(new object[] { 1,"red", "green" },  Lowercasestringsinarray.ArrayLowerCase(new object[] {1, "Red", "Green" }));
+    }
+}
