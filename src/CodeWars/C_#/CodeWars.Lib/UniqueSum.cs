dad4c0b74853d2ff138614a4b6836557c4812956
Vakhid-Dev <01.unbroken@gmail.com>
2023-08-02 19:02:12 +0300

    UniqueSum  Retrievearrayvaluebyindexwithdefault

---
+++ src/CodeWars/C_#/CodeWars.Lib/UniqueSum.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeWars.Lib;
+
+public class UniqueSumClass
+{
+    public static int? UniqueSum(List<int> lst)
+    {
+        return lst.Any() ? lst.Distinct().Sum() : null;
+    }
+}
---
+++ src/CodeWars/C_#/CodeWars.Test/RetrievearrayvaluebyindexwithdefaultTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars.Lib;
+
+namespace CodeWars.Test;
+
+public class RetrievearrayvaluebyindexwithdefaultTest
+{
+    [Test, Description("Sample Tests")]
+    public void SampleTest()
+    {
+        int[] range = Enumerable.Range(1, 3).ToArray();
+        Assert.AreEqual(-1, Retrievearrayvaluebyindexwithdefault.Solution(range, 5, -1));
+        Assert.AreEqual(2, Retrievearrayvaluebyindexwithdefault.Solution(range, 1, -1));
+        Assert.AreEqual(3, Retrievearrayvaluebyindexwithdefault.Solution(range, -1, -1));
+        Assert.AreEqual(-1, Retrievearrayvaluebyindexwithdefault.Solution(range, -5, -1));
+        Assert.AreEqual(1, Retrievearrayvaluebyindexwithdefault.Solution(range, -3, -1));
+    }
+}
