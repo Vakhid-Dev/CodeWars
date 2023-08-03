c8fd6c3c81f1a810e531576e28f8b80910c982d4
Vakhid-Dev <01.unbroken@gmail.com>
2023-08-03 21:07:03 +0300

    CapMe

---
+++ src/CodeWars/C_#/CodeWars.Test/NameArrayCappingTest.cs
@@ -0,0 +1,12 @@
+﻿using CodeWars.Lib;
+
+namespace CodeWars.Test;
+
+public class NameArrayCappingTest
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.AreEqual(new[] {"Expected"}, NameArrayCapping.CapMe2(new[] {"ExPeCteD"}));
+    }
+}
