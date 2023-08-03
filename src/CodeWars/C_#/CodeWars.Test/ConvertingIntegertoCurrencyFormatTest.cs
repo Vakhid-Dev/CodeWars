e47ab1504e5057d77ba7ba405efb0e78c6bf8dcc
Vakhid-Dev <01.unbroken@gmail.com>
2023-08-03 21:45:42 +0300

    ToCurrency

---
+++ src/CodeWars/C_#/CodeWars.Test/ConvertingIntegertoCurrencyFormatTest.cs
@@ -0,0 +1,14 @@
+﻿using CodeWars.Lib;
+
+namespace CodeWars.Test;
+
+public class ConvertingIntegertoCurrencyFormatTest
+{
+    [Test]
+    [TestCase(100000, ExpectedResult="100,000")]
+    [TestCase(5678545, ExpectedResult="5,678,545")]
+    public static string FixedTest(int price)
+    {
+        return ConvertingIntegertoCurrencyFormat.ToCurrency3(price);
+    }
+}
