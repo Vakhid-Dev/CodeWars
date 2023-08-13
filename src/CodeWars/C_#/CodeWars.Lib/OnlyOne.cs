b4bed966733608ec2fc7507b1384973a44aaa2ce
Vakhid-Dev <01.unbroken@gmail.com>
2023-08-13 14:12:25 +0300

    OnlyOne

---
+++ src/CodeWars/C_#/CodeWars.Lib/OnlyOne.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeWars.Lib;
+
+public class OnlyOne1
+{
+    public static bool OnlyOne(params bool[] flags)
+    {
+        if (flags.Length == 0) return false;
+        return flags.Where(x=> x).Count()==1;
+    }
+}
---
+++ src/CodeWars/C_#/CodeWars.Test/OnlyOneTest.cs
@@ -0,0 +1,12 @@
+﻿using CodeWars.Lib;
+
+namespace CodeWars.Test;
+
+public class OnlyOneTest
+{
+    [Test]
+    public void BasicTest()
+    {
+        Assert.AreEqual(true, OnlyOne1.OnlyOne(true, false));
+    }
+}
