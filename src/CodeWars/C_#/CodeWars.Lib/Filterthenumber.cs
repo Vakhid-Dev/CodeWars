d116f199b31c911a19ee1723cd11525d47931cfa
Vakhid-Dev <01.unbroken@gmail.com>
2023-08-13 13:57:28 +0300

    Filterthenumber

---
+++ src/CodeWars/C_#/CodeWars.Lib/Filterthenumber.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CodeWars.Lib;
+
+public class Filterthenumber
+{
+    public static int FilterString(string s)
+    {
+        StringBuilder builder = new StringBuilder();
+        var regex = new Regex(@"\d");
+        MatchCollection matches = regex.Matches(s);
+        if (matches.Count > 0)
+        {
+            foreach (Match match in matches)
+                builder.Append(match.Value); 
+        }
+        
+        return int.Parse(builder.ToString());
+    }
+
+        public static int FilterString2(string s)
+        {
+            return int.Parse(new string(s.Where(char.IsDigit).ToArray()));
+        }
+}
