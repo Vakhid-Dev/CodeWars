4714e6214edb0ad5a58ef764464eaf4ce285ebb0
Vakhid-Dev <01.unbroken@gmail.com>
2023-08-14 11:32:02 +0300

    SortGiftCode

---
+++ src/CodeWars/C_#/CodeWars.Test/SorttheGiftCodeTest.cs
@@ -0,0 +1,14 @@
+﻿using CodeWars.Lib;
+
+namespace CodeWars.Test;
+
+public class SorttheGiftCodeTest
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.AreEqual("abcdef", SorttheGiftCode.SortGiftCode("abcdef"));
+        Assert.AreEqual("kpqsuvy", SorttheGiftCode.SortGiftCode("pqksuvy"));
+        Assert.AreEqual("abcdefghijklmnopqrstuvwxyz", SorttheGiftCode.SortGiftCode("zyxwvutsrqponmlkjihgfedcba"));
+    }
+}
