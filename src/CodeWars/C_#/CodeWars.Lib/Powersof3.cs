383c635b71719f779de10dc3dfe5affdaed2dbcd
Vakhid-Dev <01.unbroken@gmail.com>
2023-08-14 11:54:30 +0300

    LargestPower

---
+++ src/CodeWars/C_#/CodeWars.Lib/Powersof3.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeWars.Lib;
+
+public class Powersof3
+{
+    public static int LargestPower(int n)
+    {
+        int result = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if (Math.Pow(3, i) < n)
+            {
+                result = i;
+            }
+        }
+
+        return result;
+    }
+    public static int LargestPower2(int n) {
+        int k = 0;  
+        while(Math.Pow(3,k) < n) {
+            k++;
+        }
+        return k-1;    
+    }
+    public static int LargestPower3(int n) => (int)Math.Ceiling(Math.Log(n, 3)) - 1;
+}
---
+++ src/CodeWars/C_#/CodeWars.Test/Powersof3Test.cs
@@ -0,0 +1,13 @@
+﻿using CodeWars.Lib;
+
+namespace CodeWars.Test;
+
+public class Powersof3Test
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.AreEqual(0, Powersof3.LargestPower(3));
+        Assert.AreEqual(1, Powersof3.LargestPower(4));
+    }
+}
