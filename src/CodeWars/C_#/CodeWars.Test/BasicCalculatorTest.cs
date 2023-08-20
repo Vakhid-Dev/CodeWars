ef6ed67f354197e28d926090a402e77ef61811e4
Vakhid-Dev <01.unbroken@gmail.com>
2023-08-20 17:44:43 +0300

    BasicCalculator

---
+++ src/CodeWars/C_#/CodeWars.Test/BasicCalculatorTest.cs
@@ -0,0 +1,36 @@
+﻿using CodeWars.Lib;
+
+namespace CodeWars.Test;
+
+public class BasicCalculatorTest
+{
+    [Test]
+    public void Test_01()
+    {
+        Assert.AreEqual(11.2, BasicCalculator.Execute(3.2,'+', 8));
+    }
+    
+    [Test]
+    public void Test_02()
+    {
+        Assert.AreEqual(-4.8, BasicCalculator.Execute(3.2,'-', 8));
+    }
+    
+    [Test]
+    public void Test_03()
+    {
+        Assert.AreEqual(0.4, BasicCalculator.Execute(3.2,'/', 8));
+    }
+    
+    [Test]
+    public void Test_04()
+    {
+        Assert.AreEqual(25.6, BasicCalculator.Execute(3.2,'*', 8));
+    }
+    
+    [Test]
+    public void Test_10()
+    {
+        Assert.Throws<ArgumentException>(() => BasicCalculator.Execute(-3,'w', 1));
+    }
+}
