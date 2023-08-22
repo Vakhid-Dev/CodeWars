3694b9d99361a12eb1ce1ce48ea8c9cd885c660f
Vakhid-Dev <01.unbroken@gmail.com>
2023-08-22 10:03:20 +0300

    ProductOfMaximumsOfArray_Array_Series__2_

---
+++ src/CodeWars/C_#/CodeWars.Lib/ProductOfMaximumsOfArray(Array Series #2).cs
@@ -0,0 +1,9 @@
+﻿namespace CodeWars.Lib;
+
+public class ProductOfMaximumsOfArray_Array_Series__2_
+{
+    public static int MaxProduct(int[] arr, int size)
+    {
+        return arr.OrderByDescending(x=>x).Take(size).Aggregate((x, y) => x * y);
+    }
+}
