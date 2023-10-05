3a88d25c514dbbfd35375c18d76861e523f5dfaa
Vakhid-Dev <01.unbroken@gmail.com>
2023-10-05 14:12:47 +0300

    6kuy_Multiplesof3or5

---
+++ src/CodeWars/C_#/CodeWars.Lib/6kuy/Multiplesof3or5.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWars.Lib._6kuy;
+
+public class Multiplesof3or5
+{
+    public static int Solution(int value)
+    {
+        return value < 0 ? 0 :
+                Enumerable.Range(0, value).
+                Sum(x => x % 3 == 0 || x % 5 == 0 ? x:0);
+    }
+    public static int Solution2(int value)
+    {
+        var sum = 0;
+        for(int i = 3; i < value; i++)
+        {
+            if(i % 3 == 0 || i % 5 == 0) sum += i;
+        }
+        return sum;
+    }
+}
