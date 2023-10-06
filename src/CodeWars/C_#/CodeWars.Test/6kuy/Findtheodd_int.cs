acaad40002630f4b5cfeb3c1dc7c9bd9798ac6ce
Vakhid-Dev <01.unbroken@gmail.com>
2023-10-06 18:06:26 +0300

    Findtheodd_int

---
+++ src/CodeWars/C_#/CodeWars.Test/6kuy/Findtheodd_int.cs
@@ -0,0 +1,14 @@
+﻿using CodeWars.Lib._6kuy;
+
+namespace CodeWars.Test._6kuy;
+
+public class Findtheodd_intTest
+{
+    [Test]
+    public void Tests()
+    {
+        Assert.That(Findtheodd_int.find_it ( new[] { 1,1,2 }), Is.EqualTo(2));
+        Assert.That(Findtheodd_int.find_it ( new[] { 1,2,2,3,3,3,4,3,3,3,2,2,1 }), Is.EqualTo(4));
+        Assert.AreEqual(5 , Findtheodd_int.find_it ( new[] { 20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5 }));
+    }
+}
