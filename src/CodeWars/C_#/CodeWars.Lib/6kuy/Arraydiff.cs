e86b6eec05b16b0df25e190b914d65b9510915c3
Vakhid-Dev <01.unbroken@gmail.com>
2023-10-12 18:06:07 +0300

    Arraydiff

---
+++ src/CodeWars/C_#/CodeWars.Lib/6kuy/Arraydiff.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeWars.Lib._6kuy;
+
+public class Arraydiff
+{
+    public static int[] ArrayDiff(int[] a, int[] b)
+    {
+        return  a.Where(x => !b.Contains(x)).ToArray();
+    }
+}
---
+++ src/CodeWars/C_#/CodeWars.Test/6kuy/ArraydiffTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars.Lib._6kuy;
+
+namespace CodeWars.Test._6kuy;
+
+public class ArraydiffTest
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.AreEqual(new int[] { 2 }, Arraydiff.ArrayDiff(new int[] { 1, 2 }, new int[] { 1 }));
+        Assert.AreEqual(new int[] { 2, 2 }, Arraydiff.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 1 }));
+        Assert.AreEqual(new int[] { 1 }, Arraydiff.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 2 }));
+        Assert.AreEqual(new int[] { 1, 2, 2 }, Arraydiff.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { }));
+        Assert.AreEqual(new int[] { }, Arraydiff.ArrayDiff(new int[] { }, new int[] { 1, 2 }));
+
+    }
+}
