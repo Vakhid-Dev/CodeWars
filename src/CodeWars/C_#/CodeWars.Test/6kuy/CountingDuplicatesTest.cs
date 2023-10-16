faec6339a0a25a8e0e59569a4b2702f42b447ddf
Vakhid-Dev <01.unbroken@gmail.com>
2023-10-16 16:54:30 +0300

    CountingDuplicates

---
+++ src/CodeWars/C_#/CodeWars.Test/6kuy/CountingDuplicatesTest.cs
@@ -0,0 +1,17 @@
+﻿using CodeWars.Lib._6kuy;
+
+namespace CodeWars.Test._6kuy;
+
+public class CountingDuplicatesTest
+{
+    [Test]
+    public void KataTests()
+    {
+        Assert.AreEqual(0, CountingDuplicates.DuplicateCount(""));
+        Assert.AreEqual(0, CountingDuplicates.DuplicateCount("abcde"));
+        Assert.AreEqual(2, CountingDuplicates.DuplicateCount("aabbcde"));
+        Assert.AreEqual(2, CountingDuplicates.DuplicateCount("aabBcde"), "should ignore case");
+        Assert.AreEqual(1, CountingDuplicates.DuplicateCount("Indivisibility"));
+        Assert.AreEqual(2, CountingDuplicates.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+    }
+}
