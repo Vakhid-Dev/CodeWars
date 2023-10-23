3ed4d211b0a1345724aa44f44a33745f15f5ee5e
Vakhid-Dev <01.unbroken@gmail.com>
2023-10-23 15:49:59 +0300

    DuplicateEncode

---
+++ src/CodeWars/C_#/CodeWars.Lib/6kuy/DuplicateEncoder.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace CodeWars.Lib._6kuy;
+
+public class DuplicateEncoder
+{
+    public static string DuplicateEncode(string word)
+    {
+        var builder = new StringBuilder();
+        var strs = word.Select(x=>x.ToString().ToUpperInvariant()).ToList();
+        foreach (var x in strs)
+        {
+            builder.Append(strs.Count(c => c == x)>1? ")" : "(");
+        }
+        
+        return builder.ToString();
+    }
+    public static string DuplicateEncode2(string word)
+    {
+        word = word.ToLower();
+        return string.Concat(word.Select(x => word.Count(w => w == x) > 1 ? ')' : '('));
+    }
+}
---
+++ src/CodeWars/C_#/CodeWars.Test/6kuy/DuplicateEncoderTest.cs
@@ -0,0 +1,15 @@
+﻿using CodeWars.Lib._6kuy;
+
+namespace CodeWars.Test._6kuy;
+
+public class DuplicateEncoderTest
+{
+    [Test]
+    public void BasicTests()
+    {
+        Assert.AreEqual("(((", DuplicateEncoder.DuplicateEncode("din"));
+        Assert.AreEqual("()()()", DuplicateEncoder.DuplicateEncode("recede"));
+        Assert.AreEqual(")())())", DuplicateEncoder.DuplicateEncode("Success"), "should ignore case");
+        Assert.AreEqual("))((", DuplicateEncoder.DuplicateEncode("(( @"));
+    }
+}
