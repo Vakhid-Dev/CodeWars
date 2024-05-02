ab0678d1f92d113343a626f1b2d6ae7f59c98a43
Vakhid-Dev <01.unbroken@gmail.com>
2024-05-02 08:46:42 +0300

    Add test

--- src/CodeWars/C_#/CodeWars.Test/6kuy/TakeaTenMinutesWalkTest.cs
+++ src/CodeWars/C_#/CodeWars.Test/6kuy/TakeaTenMinutesWalkTest.cs
@@ -8,7 +8,7 @@ public class TakeaTenMinutesWalkTest
     public void SampleTest()
     {
         Assert.AreEqual(true, TakeaTenMinutesWalk.IsValidWalk(new string[] {"n","s","n","s","n","s","n","s","n","s"}), "should return true");
-       Assert.AreEqual(false, TakeaTenMinutesWalk.IsValidWalk(new string[] {"w","e","w","e","w","e","w","e","w","e","w","e"}), "should return false");
+        Assert.AreEqual(false, TakeaTenMinutesWalk.IsValidWalk(new string[] {"w","e","w","e","w","e","w","e","w","e","w","e"}), "should return false");
         Assert.AreEqual(false, TakeaTenMinutesWalk.IsValidWalk(new string[] {"w"}), "should return false");
         Assert.AreEqual(false, TakeaTenMinutesWalk.IsValidWalk(new string[] {"n","n","n","s","n","s","n","s","n","s"}), "should return false");
     }
