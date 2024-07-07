5bf4da74788c557b650dd30c61ea2ce147b3b757
vaxid.guliev <vaxid.guliev@irlix.ru>
2024-07-07 21:05:41 +0300

    Added add case

--- src/CodeWars/C_#/CodeWars.Test/6kuy/PartsSumsClassTest.cs
+++ src/CodeWars/C_#/CodeWars.Test/6kuy/PartsSumsClassTest.cs
@@ -12,7 +12,7 @@ public class PartsSumsClassTest
     public static void atest0() 
     {        
         Console.WriteLine("Basic Tests");
-      //  dotest(new int[] {}, new int[] {0});
+        dotest(new int[] {}, new int[] {0});
         dotest(new int[] {0, 1, 3, 6, 10}, new int[] {20, 20, 19, 16, 10, 0});
         dotest(new int[] {1, 2, 3, 4, 5, 6}, new int[] {21, 20, 18, 15, 11, 6, 0});        
         dotest(new int[] {744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358}, 
